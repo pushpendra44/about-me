992bfd58526cbf2e5375687cdeee87e8ab28eb84
pushpendra44 <71849199+pushpendra44@users.noreply.github.com>
2025-10-26 16:16:03 +0000

    Deploying to gh-pages from @ pushpendra44/about-me@a2a6151473cfd666cca1fb8f1ffec5e41978a06b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -446,16 +446,6 @@ ninja.data = [{
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/about-me/projects/2_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/about-me/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/about-me/projects/9_project/";
             },},{
         id: 'social-email',
         title: 'email',
