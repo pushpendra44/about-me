45ccfbe3819e56fcce1aa13776c55220f01770ed
pushpendra44 <71849199+pushpendra44@users.noreply.github.com>
2025-10-26 16:14:14 +0000

    Deploying to gh-pages from @ pushpendra44/about-me@17f297f362c07ead90d991aafc8dcf99fe5985d5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -446,6 +446,26 @@ ninja.data = [{
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/about-me/projects/2_project/";
+            },},{id: "projects-project-5",
+          title: 'project 5',
+          description: "a project with a background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/about-me/projects/5_project/";
+            },},{id: "projects-project-6",
+          title: 'project 6',
+          description: "a project with no image",
+          section: "Projects",handler: () => {
+              window.location.href = "/about-me/projects/6_project/";
+            },},{id: "projects-project-7",
+          title: 'project 7',
+          description: "with background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/about-me/projects/7_project/";
+            },},{id: "projects-project-8",
+          title: 'project 8',
+          description: "an other project with a background image and giscus comments",
+          section: "Projects",handler: () => {
+              window.location.href = "/about-me/projects/8_project/";
             },},{id: "projects-project-9",
           title: 'project 9',
           description: "another project with an image 🎉",
